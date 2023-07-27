03343ba2a2edef2c01c76865b8606b63b4f15974
ArtemBli <92346129+ArtemBli@users.noreply.github.com>
2023-07-27 12:54:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const inputBox = document.getElementById("myInputBox");
+
+inputBox.addEventListener("input", function() {
+    if (inputBox.value === "") {
+        inputBox.style.color = "black"; 
+    } else {
+        inputBox.style.color = "black";
+    }
+});
